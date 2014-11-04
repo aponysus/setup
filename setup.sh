1ad23409e55fb17d685959f7db25dc679efe0ee2
jsorrell3 <jsorrell3@gatech.edu>
2014-11-04 14:01:36 -0600

    add vim setup

--- setup.sh
+++ setup.sh
@@ -16,15 +16,15 @@ if [ -d ./.vim/ ]; then
     mv .vim old_vimfiles
 fi
 
-mkdir -p .vim
 git clone https://github.com/aponysus/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.vimrc .
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
+mkdir ~/.vim
+cd ~/.vim && git init
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/.tmp
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
 git clone http://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
 git clone http://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
-git clone http://github.com/sjl/badwolf.git ~/.vim/bundle/badwolf
+git submodule add https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
